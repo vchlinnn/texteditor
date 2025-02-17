fbdc281f99030946b4d602e62ce32ef7c7a776c0
vchlinnn <chuclinh190305@gmail.com>
2025-02-16 21:14:04 -0600

    Implemented gap buffer

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -4,35 +4,53 @@ package edu.grinnell.csc207.texteditor;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
+    char[] buffer;
+    int startGap;
+    int endGap;
+
+    public GapBuffer() {
+        this.buffer = new char[10];
+        this.startGap = 0;
+        this.endGap = buffer.length - 1;
+    }
+
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        buffer[startGap] = ch;
+        startGap++;
     }
 
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        startGap--;
     }
 
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return startGap;
     }
 
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        buffer[endGap - 1] = buffer[startGap - 1];
+        startGap--;
+        endGap--;
     }
 
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        buffer[startGap] = buffer[endGap];
+        startGap++;
+        endGap++;
     }
 
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return buffer.length - (endGap - startGap);
     }
 
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        if (i < 0 || i >= buffer.length) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return buffer[i];
     }
 
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        return (new String(buffer)).substring(0, startGap) + (new String(buffer)).substring(endGap);
     }
 }
