d6740e1777a5cba9e61fe394b211562c6091d9e1
vchlinnn <chuclinh190305@gmail.com>
2025-02-21 17:00:25 -0600

    Create a screen object

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -1,5 +1,11 @@
 package edu.grinnell.csc207.texteditor;
 
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+
+import com.googlecode.lanterna.screen.Screen;
+
 /**
  * The driver for the TextEditor Application.
  */
@@ -7,15 +13,18 @@ public class TextEditor {
 
     /**
      * The main entry point for the TextEditor application.
+     * 
      * @param args command-line arguments.
+     * @throws IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: java TextEditor <filename>");
             System.exit(1);
         }
 
-        // TODO: fill me in with a text editor TUI!
+        DefaultTerminalFactory factory = new DefaultTerminalFactory();
+        Screen screen = factory.createScreen();
         String path = args[0];
         System.out.format("Loading %s...\n", path);
     }
